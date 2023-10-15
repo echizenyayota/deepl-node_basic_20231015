3af4a1bcff992d13c42ebd80ac73cb091261e592
echizenyayota <virtualmisso30@gmail.com>
2023-10-15 19:08:24 +0900

    set the code to translate using deepl-node

--- server.js
+++ server.js
@@ -7,9 +7,18 @@ const PORT = 8000;
 const axios = require("axios").default;
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
 
 app.listen(PORT, () => console.log("Server running on PORT " + PORT));
+
+const authKey = process.env.DEEPL_API_KEY; // Replace with your key
+const translator = new deepl.Translator(authKey);
+
+(async () => {
+  const result = await translator.translateText('good evening', null, "ja");
+  console.log(result.text);
+})();
