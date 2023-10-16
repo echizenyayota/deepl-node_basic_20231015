9f8b52f67cd487f3c792590dfcf98ecc3f735ba8
echizenyayota <virtualmisso30@gmail.com>
2023-10-16 12:47:40 +0900

    set the codes to show the languages to translate

--- server.js
+++ server.js
@@ -7,9 +7,19 @@ const PORT = 8000;
 const axios = require("axios").default;
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
 
 app.listen(PORT, () => console.log("Server running on PORT " + PORT));
+
+const authKey = process.env.DEEPL_API_KEY; // Replace with your key
+const translator = new deepl.Translator(authKey);
+
+const sourceLanguages = await translator.getSourceLanguages();
+for (let i = 0; i < sourceLanguages.length; i++) {
+    const lang = sourceLanguages[i];
+    console.log(`${lang.name} (${lang.code})`); // Example: 'English (en)'
+}
